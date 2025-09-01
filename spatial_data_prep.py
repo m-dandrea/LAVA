ed49df3c3c919ccc04bb523ad2055717f87faf6e
jome1 <jonas_meier@gmx.de>
2025-09-01 14:16:12 +0200

    add buffered DEM for terrain ruggedness calculation

--- spatial_data_prep.py
+++ spatial_data_prep.py
@@ -176,7 +176,7 @@ with open(os.path.join(output_dir, f'{region_name_clean}_local_CRS.pkl'), 'wb') 
 region.to_crs(local_crs_obj, inplace=True) 
 region.to_file(os.path.join(output_dir, f'{region_name_clean}_{local_crs_tag}.geojson'), driver='GeoJSON', encoding='utf-8')
 # also have region polygon in equal-area Mollweide projection
-region_mollweide = region.to_crs(CRS.from_user_input('ESRI:54009')) 
+region_mollweide = region.to_crs(CRS.from_user_input('ESRI:54009'))
 
 # set global CRS EPSG:4326
 global_crs_obj = CRS.from_user_input('4326')
@@ -189,8 +189,10 @@ with open(os.path.join(output_dir, f'{region_name_clean}_global_CRS.pkl'), 'wb')
 #calculate bounding box with 1000m buffer (region needs to be in projected CRS so meters are the unit)
 region_copy = region
 region_copy['buffered']=region_copy.buffer(1000)
+region_buffered_4000 = region_copy.buffer(4000) #have DEM larger than study area to account for Ruggedness Index calculation (convert to 4326 below)
 # Convert buffered region back to EPSC 4326 to get bounding box latitude and longitude 
 region_buffered_4326 = region_copy.set_geometry('buffered').to_crs(global_crs_obj)
+region_buffered_4000 = region_buffered_4000.to_crs(global_crs_obj)
 bounding_box = region_buffered_4326['buffered'].total_bounds 
 logging.info(f"Bounding box in EPSG 4326: \nminx: {bounding_box[0]}, miny: {bounding_box[1]}, maxx: {bounding_box[2]}, maxy: {bounding_box[3]}")
 
@@ -462,7 +464,9 @@ if config['landcover_source'] == 'file':
 print('processing DEM') #block comment: SHIFT+ALT+A, multiple line comment: STRG+#
 try:
     clip_reproject_raster(demRasterPath, region_name_clean, region, 'DEM', local_crs_obj, 'nearest', 'int16', output_dir)
+    clip_reproject_raster(demRasterPath, region_name_clean, region_buffered_4000, 'DEM_buffered', local_crs_obj, 'nearest', 'int16', output_dir)
     dem_4326_Path = os.path.join(output_dir, f'DEM_{region_name_clean}_EPSG4326.tif')
+    dem_local_buffered_Path = os.path.join(output_dir, f'DEM_buffered_{region_name_clean}_{local_crs_tag}.tif') #for ruggedness index calculation
     #reproject and match resolution of DEM to landcover data (co-registration)
     dem_localCRS_Path=os.path.join(output_dir, f'DEM_{region_name_clean}_{local_crs_tag}.tif')
     # dem_resampled_Path=os.path.join(output_dir, f'DEM_{region_name_clean}_{local_crs_tag}_resampled.tif') 
@@ -505,7 +509,7 @@ try:
         if os.path.exists(tri_local_path) and os.path.exists(tri_global_path):
             print(f"Terrain Ruggedness Index already exists at {rel_path(tri_local_path)}. Skipping calculation.")
         else:
-            dem = xdem.DEM(dem_localCRS_Path)
+            dem = xdem.DEM(dem_local_buffered_Path)
             tri = dem.terrain_ruggedness_index(window_size=9)
             tri.save(tri_local_path)
             reproject_raster(tri_local_path, region_name_clean, 4326, 'nearest', 'float32', tri_global_path)
